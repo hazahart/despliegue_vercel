7931802c912d7c748bbbb1bced2a16e6102a16ef
Gustavo Ramirez Mireles <gusramireles@hotmail.com>
2026-03-23 19:15:21 -0600

    profiles supabase

--- api/datos.js
+++ api/datos.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
     if (method === 'GET') {
         try {
             const { data, error } = await supabase
-                .from('budgets')
+                .from('profiles')
                 .select('*');
 
             if (error) {
